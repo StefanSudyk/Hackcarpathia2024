4ae72841c8eebd1488cd59c001dd087dfdafefde
Damian Świeca <damianswieca04@gmail.com>
2024-04-06 22:32:14 +0200

    segregowanie ale nie widac w ui
    i sie sciemnia potem

---
+++ Assets/LightingScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LightingScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject darkPlane;
+    public float energyLevel;
+    public float energyDecreaseRate; // Prêdkoœæ spadku energii na jednostkê czasu
+    [SerializeField] private TMP_Text energyDisplay;
+
+    void Start()
+    {
+        energyLevel = 100;
+        energyDecreaseRate = 1.5f;
+    }
+
+    private void OnGUI()
+    {
+        energyDisplay.text = energyLevel.ToString();
+    }
+
+    void Update()
+    {
+        // Zmniejszaj poziom energii z czasem
+        energyLevel -= energyDecreaseRate * Time.deltaTime;
+
+        // Ogranicz energiê do zakresu [0, 100]
+        energyLevel = Mathf.Clamp(energyLevel, 0, 100);
+        Debug.Log(energyLevel);
+
+        // Ustaw przezroczystoœæ na podstawie poziomu energii
+        if(energyLevel > 30)
+        {
+            SetTransparency(0);
+        }
+        if (energyLevel < 30 && energyLevel > 1)
+        {
+            float transparency = Mathf.Lerp(0, 1, (30 - energyLevel) / 30f); // Interpoluj przezroczystoœæ od 0 do 1 w zale¿noœci od poziomu energii
+            SetTransparency(transparency);
+        }
+    }
+
+    public void SetTransparency(float transparency)
+    {
+        Color color = darkPlane.GetComponent<SpriteRenderer>().color;
+        color.a = transparency;
+        darkPlane.GetComponent<SpriteRenderer>().color = color;
+    }
+
+    public void AddEnergy(float energy)
+    {
+        energyLevel += energy;
+    }
+}
--- Assets/TrashCollector.cs
+++ Assets/TrashCollector.cs
@@ -8,8 +8,10 @@ public class TrashCollector : MonoBehaviour
     // Start is called before the first frame update
     public static TrashCollector instance;
     private Stack<int> trashCollection = new Stack<int>();
+    public LightingScript lightScript;
 
     [SerializeField] private TMP_Text trashDisplay;
+    
     public int trash;
     private void Awake()
     {
@@ -23,11 +25,68 @@ public class TrashCollector : MonoBehaviour
         trashDisplay.text = trash.ToString();
     }
 
+    public void ThrowBlueTrash()
+    {
+        if(trashCollection.Count != 0)
+        {
+            if (trashCollection.Peek() == 2)
+            {
+                Debug.Log("Throwing away blue trash");
+                lightScript.AddEnergy(2);
+                trashCollection.Pop();
+                trash--;
+            }
+        }
+    }
+
+    public void ThrowYellowTrash()
+    {
+        if (trashCollection.Count != 0)
+        {
+            if (trashCollection.Peek() == 0)
+            {
+                Debug.Log("Throwing away yellow trash");
+                lightScript.AddEnergy(2);
+                trashCollection.Pop();
+                trash--;
+            }
+        }
+            
+    }
+
+    public void ThrowGreenTrash()
+    {
+        if (trashCollection.Count != 0)
+        {
+            if (trashCollection.Peek() == 3)
+            {
+                Debug.Log("Throwing away green trash");
+                lightScript.AddEnergy(2);
+                trashCollection.Pop();
+                trash--;
+            }
+        }
+            
+    }
+
+    public void ThrowBrownTrash()
+    {
+        if (trashCollection.Count != 0)
+        {
+            if (trashCollection.Peek() == 1)
+            {
+                Debug.Log("Throwing away brown trash");
+                lightScript.AddEnergy(2);
+                trashCollection.Pop();
+                trash--;
+            }
+        }
+            
+    }
+
     public void IncreaseTrash(int t, int tClass)
     {
         trash +=t;
         trashCollection.Push(tClass);
     }
-    // Update is called once per frame
-    
 }
